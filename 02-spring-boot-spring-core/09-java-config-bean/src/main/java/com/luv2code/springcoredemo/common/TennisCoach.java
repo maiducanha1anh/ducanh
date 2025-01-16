73daffe103c15493bcc72140ff25f2db950f9ec4
maiducanha1anh <maiducanha1anh@gmail.com>
2025-01-16 23:19:35 +0700

    feat: 64.setting up database table - coding

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,14 +1,16 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.stereotype.Component;
 
-public class SwimCoach implements Coach{
+@Component
+public class TennisCoach implements Coach {
 
-    public SwimCoach () {
+    public TennisCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
-
     @Override
     public String getDailyWorkout() {
-        return "Swim 1000 metres as a warm up";
+        return "Practice your backhand volley ";
     }
+
 }
