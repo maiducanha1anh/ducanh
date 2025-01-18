680b1d00f9ad1beb4a3e27de17215f02f0746c3b
maiducanha1anh <maiducanha1anh@gmail.com>
2025-01-18 20:00:26 +0700

    feat: 77. reading objects with jpa - coding

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -19,8 +19,30 @@ public class CruddemoApplication {
 		return runner -> {
 			//createStudent(studentDAO);
 
-			createMultipleStudents(studentDAO);
+			//createMultipleStudents(studentDAO);
+
+			reafStudent(studentDAO);
 		};
+	}
+
+	private void reafStudent(StudentDAO studentDAO) {
+
+		// create a student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Daffy","Duck","daffy@luv2code.com");
+
+		// save the student
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+		//retrieve student based on the id: primary key
+		System.out.println("retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+		// display student
+		System.out.println("Found the student: " + myStudent);
 	}
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
