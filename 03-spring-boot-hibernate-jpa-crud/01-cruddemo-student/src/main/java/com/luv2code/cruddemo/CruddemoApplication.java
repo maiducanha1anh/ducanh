cfba54d6b278eb0f807e9f1598c8a766a4674425
maiducanha1anh <maiducanha1anh@gmail.com>
2025-01-18 20:35:11 +0700

    feat: 79. querying objects with jpa - coding - part 1

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -21,11 +23,27 @@ public class CruddemoApplication {
 
 			//createMultipleStudents(studentDAO);
 
-			reafStudent(studentDAO);
+			//readStudent(studentDAO);
+
+			queryForStudents(studentDAO) ;
+
 		};
 	}
 
-	private void reafStudent(StudentDAO studentDAO) {
+	private void queryForStudents(StudentDAO studentDAO) {
+
+		// get a list students
+		List<Student> theStudents = studentDAO.findALl();
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+
+	}
+
+
+	private void readStudent(StudentDAO studentDAO) {
 
 		// create a student object
 		System.out.println("Creating new student object ...");
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Repository
@@ -34,9 +37,15 @@ public class StudentDAOImpl implements StudentDAO{
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id) ;
     }
+
+    @Override
+    public List<Student> findALl() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName ", Student.class);
+
+        // return query results
+        return theQuery.getResultList() ;
+    }
 }
-
-
-
 
 
